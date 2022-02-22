ef8b2c40e124309b9cb2c831abc6eed0e78462ad
Tigris Mendez <70561045+uploadtigris@users.noreply.github.com>
2022-02-22 00:18:45 -0600

    Update modal-window.js

--- modal-window.js
+++ modal-window.js
@@ -3,22 +3,32 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-    btnsOpenModal[i].addEventListener('click', function () {
-        console.log('button clicked');
-        modal.classList.remove('hidden');
-        //do not use a '.' selector as you are just passing hidden.
-        overlay.classList.remove('hidden');
-    });
+const openModal = function () {
+    modal.classList.remove('hidden');
+    //do not use a '.' selector as you are just passing hidden.
+    overlay.classList.remove('hidden');
+}
 
 const closeModal = function () {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
 }
 
+for (let i = 0; i < btnsOpenModal.length; i++)
+    btnsOpenModal[i].addEventListener('click', openModal);
+
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 //closeModal function executes only after the btn is clicked.
 
+document.addEventListener('keydown', function (e) {
+    console.log(e.key)
+
+
+    if (e.key === 'Escape') {
+        if (!modal.classList.contains('hidden')) {
+            closeModal();
+        }
+    }
+})
